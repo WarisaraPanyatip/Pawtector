2016b6842663f0ab5821905b79dbab3290f73710
WarisaraPanyatip <6542108126@student.chula.ac.th>
2025-04-10 14:18:28 +0700

    Initial Commit

---
+++ Pawtector/PawtectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawtectorApp.swift
+//  Pawtector
+//
+//  Created by วริศรา ปัญญาทิพย์ on 10/4/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct PawtectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
