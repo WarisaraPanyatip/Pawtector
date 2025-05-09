9b5e5442d935a5cf36a0fbf82b4cc46cc7753b4c
braibrua <piyathida.baibua@gmail.com>
2025-05-09 14:11:16 +0700

    Fix Navbar + add more features in pet

---
+++ Pawtector/TabView.swift
@@ -0,0 +1,18 @@
+//
+//  TabView.swift
+//  Pawtector
+//
+//  Created by Piyathida Changsuwan on 9/5/2568 BE.
+//
+
+import SwiftUI
+
+struct TabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabView()
+}
