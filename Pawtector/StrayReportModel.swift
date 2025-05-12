7eab570f3e5d0c582c7f9bf9ab96143c61af9326
braibrua <piyathida.baibua@gmail.com>
2025-05-12 10:21:38 +0700

    stray report

---
+++ Pawtector/StrayReportModel.swift
@@ -0,0 +1,18 @@
+//
+//  StrayReportModel.swift
+//  Pawtector
+//
+//  Created by Piyathida Changsuwan on 12/5/2568 BE.
+//
+
+import SwiftUI
+
+struct StrayReportModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StrayReportModel()
+}
